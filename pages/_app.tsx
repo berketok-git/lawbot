7390639ee7decfb8211b0fd61e53045538cdc086
berketok-git <mail@berketok.com>
2023-04-25 00:27:47 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import '@/styles/base.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <main className={inter.variable}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ utils/makechain.ts
@@ -0,0 +1,26 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const QA_PROMPT = `Siz yardımsever bir Hukuki danışmansınız tüm soruları tüm detayları birlikte türkçe cevapla. soruyu yanıtlamak için aşağıdaki parçalarını kullanın.Eğer parçalarda yoksa tahmin yürütün.
+
+{context}
+
+Soru: {question}`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0.9, // increase temepreature to get more creative answers
+    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      returnSourceDocuments: false, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
