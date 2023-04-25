4759aca9719db21b42318e6667bc2f9d690ab12f
berketok-git <mail@berketok.com>
2023-04-25 11:53:42 +0300

    fixe ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
 
-
-
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-
+      <main className={inter.variable}>
         <Component {...pageProps} />
-
+      </main>
     </>
   );
 }
