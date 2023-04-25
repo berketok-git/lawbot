1c6dd1bf6ed4bc01a4b11a05335982564c267b92
berketok-git <mail@berketok.com>
2023-04-25 11:29:06 +0300

    fix 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
-
-
-
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
