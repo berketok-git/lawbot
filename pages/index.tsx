4c92e1e3320e4d603f612a6ae6cc88160aabcf8e
berketok-git <mail@berketok.com>
2023-04-25 11:46:47 +0300

    fine

--- pages/index.tsx
+++ pages/index.tsx
@@ -116,121 +116,113 @@ export default function Home() {
 
   return (
     <>
-      
-
-          <div className="mx-auto flex flex-col gap-4">
-            
-            <div className="mx-auto flex flex-col gap-4">
-              <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-                Hukuki Danışman
-              </h1>
-              <main className={styles.main}>
-                <div className={styles.cloud}>
-                  <div ref={messageListRef} className={styles.messagelist}>
-                    {messages.map((message, index) => {
-                      let icon;
-                      let className;
-                      if (message.type === 'apiMessage') {
-                        icon = (
-                          <Image
-                            key={index}
-                            src="/bot-image.png"
-                            alt="AI"
-                            width="40"
-                            height="40"
-                            className={styles.boticon}
-                            priority
-                          />
-                        );
-                        className = styles.apimessage;
-                      } else {
-                        icon = (
-                          <Image
-                            key={index}
-                            src="/usericon.png"
-                            alt="Me"
-                            width="30"
-                            height="30"
-                            className={styles.usericon}
-                            priority
-                          />
-                        );
-                        // The latest message sent by the user will be animated while waiting for a response
-                        className =
-                          loading && index === messages.length - 1
-                            ? styles.usermessagewaiting
-                            : styles.usermessage;
-                      }
-                      return (
-                        <>
-                          <div
-                            key={`chatMessage-${index}`}
-                            className={className}
-                          >
-                            {icon}
-                            <div className={styles.markdownanswer}>
-                              <ReactMarkdown linkTarget="_blank">
-                                {message.message}
-                              </ReactMarkdown>
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                </div>
-                <div className={styles.center}>
-                  <div className={styles.cloudform}>
-                    <form onSubmit={handleSubmit}>
-                      <textarea
-                        disabled={loading}
-                        onKeyDown={handleEnter}
-                        ref={textAreaRef}
-                        autoFocus={false}
-                        rows={1}
-                        id="userInput"
-                        name="userInput"
-                        placeholder={
-                          loading ? 'Cevap bekleniyor...' : 'Sorunuz nedir?'
-                        }
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                        className={styles.textarea}
+      <div className="mx-auto flex flex-col gap-4" >
+        <div className="mx-auto flex flex-col gap-4">
+          <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
+            Hukuki Danışman
+          </h1>
+          <main className={styles.main}>
+            <div className={styles.cloud}>
+              <div ref={messageListRef} className={styles.messagelist}>
+                {messages.map((message, index) => {
+                  let icon;
+                  let className;
+                  if (message.type === 'apiMessage') {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/bot-image.png"
+                        alt="AI"
+                        width="40"
+                        height="40"
+                        className={styles.boticon}
+                        priority
                       />
-                      <button
-                        type="submit"
-                        disabled={loading}
-                        className={styles.generatebutton}
-                      >
-                        {loading ? (
-                          <div className={styles.loadingwheel}>
-                            <LoadingDots color="#000" />
-                          </div>
-                        ) : (
-                          // Send icon SVG in input field
-                          <svg
-                            viewBox="0 0 20 20"
-                            className={styles.svgicon}
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                          </svg>
-                        )}
-                      </button>
-                    </form>
-                  </div>
-                </div>
-                {error && (
-                  <div className="border border-red-400 rounded-md p-4">
-                    <p className="text-red-500">{error}</p>
-                  </div>
-                )}
-              </main>
+                    );
+                    className = styles.apimessage;
+                  } else {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/usericon.png"
+                        alt="Me"
+                        width="30"
+                        height="30"
+                        className={styles.usericon}
+                        priority
+                      />
+                    );
+                    // The latest message sent by the user will be animated while waiting for a response
+                    className =
+                      loading && index === messages.length - 1
+                        ? styles.usermessagewaiting
+                        : styles.usermessage;
+                  }
+                  return (
+                    <>
+                      <div key={`chatMessage-${index}`} className={className}>
+                        {icon}
+                        <div className={styles.markdownanswer}>
+                          <ReactMarkdown linkTarget="_blank">
+                            {message.message}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
-            <footer className="m-auto p-4">
-              Knowledge base for Turkish Law
-            </footer>
-          </div>
+            <div className={styles.center}>
+              <div className={styles.cloudform}>
+                <form onSubmit={handleSubmit}>
+                  <textarea
+                    disabled={loading}
+                    onKeyDown={handleEnter}
+                    ref={textAreaRef}
+                    autoFocus={false}
+                    rows={1}
+                    id="userInput"
+                    name="userInput"
+                    placeholder={
+                      loading ? 'Cevap bekleniyor...' : 'Sorunuz nedir?'
+                    }
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className={styles.textarea}
+                  />
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className={styles.generatebutton}
+                  >
+                    {loading ? (
+                      <div className={styles.loadingwheel}>
+                        <LoadingDots color="#000" />
+                      </div>
+                    ) : (
+                      // Send icon SVG in input field
+                      <svg
+                        viewBox="0 0 20 20"
+                        className={styles.svgicon}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </div>
+            {error && (
+              <div className="border border-red-400 rounded-md p-4">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </main>
+        </div>
+        <footer className="m-auto p-4">Knowledge base for Turkish Law</footer>
+      </div>
     </>
   );
 }
