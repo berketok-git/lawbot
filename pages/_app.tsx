502a5cebb640fb8102920fcb4ecc0fb2e62164cf
berketok-git <mail@berketok.com>
2023-07-12 02:02:26 +0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
-import "@aws-amplify/ui-react/styles.css";
-import { Amplify } from "aws-amplify";
-import awsExports from "../aws";
+
 
 
 const inter = Inter({
@@ -11,7 +9,6 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
