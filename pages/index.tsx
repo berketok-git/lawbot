0288c599222dd8f221a391c75d3ef71786a0d841
berketok-git <mail@berketok.com>
2023-07-12 02:17:20 +0300

    update for prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,8 +74,6 @@ export default function Home() {
         }),
       });
       const data = await response.json();
-      console.log('data', data);
-
       if (data.error) {
         setError(data.error);
       } else {
@@ -92,17 +90,13 @@ export default function Home() {
           history: [...state.history, [question, data.text]],
         }));
       }
-      console.log('messageState', messageState);
-
       setLoading(false);
 
       //scroll to bottom
       messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
     } catch (error) {
       setLoading(false);
-      setError('An error occurred while fetching the data. Please try again.');
-      console.log('error', error);
-    }
+      setError('An error occurred while fetching the data. Please try again.');    }
   }
 
   //prevent empty submissions
